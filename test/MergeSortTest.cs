090ce11e444bac61de23753dfbe0a348e796e238
Natalia Gonzalez <nataliaisagonzalez26@gmail.com>
2020-02-16 20:33:08 -0500

    MergeSortTest method created

--- test/MergeSortTest.cs
+++ test/MergeSortTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using sorting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace test
 {
@@ -8,15 +9,72 @@ namespace test
     public class MergeSortTest
     {
         private MergeSort merge;
-        private int[] testArray;
+        private String[] testArray;
+
+        public const String NAME_FILE_Aleatorio_10_2 = "Aleatorio-10^2";
+        public const String NAME_FILE_Aleatorio_10_5 = "Aleatorio-10^5";
+        public const String NAME_FILE_Aleatorio_10_8 = "Aleatorio-10^8";
+        public const String NAME_FILE_Orden_Ascendente_10_2 = "Orden-Ascendente-10^2";
+        public const String NAME_FILE_Orden_Ascendente_10_5 = "Orden-Ascendente-10^5";
+        public const String NAME_FILE_Orden_Ascendente_10_8 = "Orden-Ascendente-10^8";
+        public const String NAME_FILE_Orden_Descendente_10_2 = "Orden-Ascendente-10^2";
+        public const String NAME_FILE_Orden_Descendente_10_5 = "Orden-Ascendente-10^5";
+        public const String NAME_FILE_Orden_Descendente_10_8 = "Orden-Ascendente-10^8";
+
+        public const String REPORT_ROUT = "..\\..\\..\\data\\Report.txt";
 
         public void setUpScenary1() {
-
+            
         }
 
         public void setUpScenary2() {
             merge = new MergeSort();
-            testArray = new int[] { 1, 4, 2, 0, 15, 8, 20, 7 };
+            testArray = new String[] { "1", "4", "2", "0", "15", "8", "20", "7"};
+        }
+
+        public void setUpScenary3()
+        {
+            Reader(NAME_FILE_Aleatorio_10_2);
+        }
+
+        public void setUpScenary4()
+        {
+            Reader(NAME_FILE_Aleatorio_10_5);
+        }
+
+        public void setUpScenary5()
+        {
+            Reader(NAME_FILE_Aleatorio_10_8);
+        }
+
+        public void setUpScenary6()
+        {
+            Reader(NAME_FILE_Orden_Ascendente_10_2);
+        }
+
+        public void setUpScenary7()
+        {
+            Reader(NAME_FILE_Orden_Ascendente_10_5);
+        }
+
+        public void setUpScenary8()
+        {
+            Reader(NAME_FILE_Orden_Ascendente_10_8);
+        }
+
+        public void setUpScenary9()
+        {
+            Reader(NAME_FILE_Orden_Descendente_10_2);
+        }
+
+        public void setUpScenary10()
+        {
+            Reader(NAME_FILE_Orden_Descendente_10_5);
+        }
+
+        public void setUpScenary11()
+        {
+            Reader(NAME_FILE_Orden_Descendente_10_8);
         }
 
         [TestMethod]
@@ -35,6 +93,340 @@ namespace test
             for(int i = 0; i < testArray.Length; i++) 
             {
                 Assert.IsTrue(testArray[i] < testArray[i + 1]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime1()
+        {
+            String report = "TestMergeSortTime1\n setUpSceneary3\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary3();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" +total.ToString()+"\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime2()
+        {
+            String report = "TestMergeSortTime2\n setUpSceneary4\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary4();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime3()
+        {
+            String report = "TestMergeSortTime3\n setUpSceneary5\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary5();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime4()
+        {
+            String report = "TestMergeSortTime4\n setUpSceneary6\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary6();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime5()
+        {
+            String report = "TestMergeSortTime5\n setUpSceneary7\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary7();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime6()
+        {
+            String report = "TestMergeSortTime6\n setUpSceneary8\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary8();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime7()
+        {
+            String report = "TestMergeSortTime7\n setUpSceneary9\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary9();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime8()
+        {
+            String report = "TestMergeSortTime8\n setUpSceneary10\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary10();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+           cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        [TestMethod]
+        public void TestMergeSortTime9()
+        {
+            String report = "TestMergeSortTime9\n setUpSceneary11\n";
+            String[] cloneArray = (String[])testArray.Clone();
+            setUpScenary11();
+            // Inicia el contador:
+            DateTime tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 1 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            cloneArray = (String[])testArray.Clone();
+
+            // Inicia el contador:
+            tiempo1 = DateTime.Now;
+
+            merge.mergeSort(cloneArray, 0, cloneArray.Length - 1);
+
+            // Para el contador e imprime el resultado:
+            tiempo2 = DateTime.Now;
+            total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+            report = report + "Prueba 2 - Tiempo de ejecución :" + total.ToString() + "\n";
+
+            Console.Write(report);
+            Writter(report);
+        }
+
+        private void Reader(String Namefile)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("..\\..\\..\\" + Namefile + ".txt");
+                testArray = sr.ReadToEnd().Split(' ');
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        public void Writter(String report)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(REPORT_ROUT, true);
+                sw.Write(report);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
             }
         }
     }
--- writer/Writer.cs
+++ writer/Writer.cs
@@ -5,11 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace writter
+namespace writer
 {
-    class Writter
+    public class Writer
     {
-
         public const String NAME_FILE_Aleatorio_10_2 = "Aleatorio-10^2";
         public const String NAME_FILE_Aleatorio_10_5 = "Aleatorio-10^5";
         public const String NAME_FILE_Aleatorio_10_8 = "Aleatorio-10^8";
@@ -20,39 +19,34 @@ namespace writter
         public const String NAME_FILE_Orden_Descendente_10_5 = "Orden-Ascendente-10^5";
         public const String NAME_FILE_Orden_Descendente_10_8 = "Orden-Ascendente-10^8";
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Inicia");
-            Writter1(NAME_FILE_Aleatorio_10_2, 100);
-            Writter1(NAME_FILE_Aleatorio_10_5, 100000);
-            Writter1(NAME_FILE_Aleatorio_10_8, 100000000);
 
-            Writter2(NAME_FILE_Orden_Ascendente_10_2, 100, true);
-            Writter2(NAME_FILE_Orden_Ascendente_10_5, 100000, true);
-            Writter2(NAME_FILE_Orden_Ascendente_10_8, 100000000, true);
+        [STAThread]
+            static void Main(string[] args)
+            {
+                Writter(NAME_FILE_Aleatorio_10_2, 100);
+                Writter(NAME_FILE_Aleatorio_10_5, 100000);
+                Writter(NAME_FILE_Aleatorio_10_8, 100000000);
 
-            Writter2(NAME_FILE_Orden_Descendente_10_2, 100, false);
-            Writter2(NAME_FILE_Orden_Descendente_10_5, 100000, false);
-            Writter2(NAME_FILE_Orden_Descendente_10_8, 100000000, false);
+                Writter(NAME_FILE_Orden_Ascendente_10_2, 100, true);
+                Writter(NAME_FILE_Orden_Ascendente_10_5, 100000, true);
+                Writter(NAME_FILE_Orden_Ascendente_10_8, 100000000, true);
 
-            Console.WriteLine("Termino");
-            Console.Read();
-
+                Writter(NAME_FILE_Orden_Descendente_10_2, 100, false);
+                Writter(NAME_FILE_Orden_Descendente_10_5, 100000, false);
+                Writter(NAME_FILE_Orden_Descendente_10_8, 100000000, false);
         }
 
-        public static void Writter1(String fileName, int size)
-        {
+        public static void Writter(String fileName, int size) { 
             try
             {
 
-                StreamWriter sw = new StreamWriter("..\\..\\..\\Data\\" + fileName + ".txt", true);
+                StreamWriter sw = new StreamWriter("..\\..\\Data\\"+fileName+".txt", true);
 
                 Random rmd = new Random();
                 for (int i = 0; i < size; i++)
                 {
-
-                    sw.Write(rmd.Next(size) + " ");
-                    Console.WriteLine("nums");
+                    
+                    sw.WriteLine(rmd.Next()*size +1+" ");
                 }
                 sw.Close();
             }
@@ -61,30 +55,28 @@ namespace writter
                 Console.WriteLine("Exception: " + e.Message);
             }
         }
-
-        public static void Writter2(String fileName, int size, Boolean order)
-        {
+        
+            public static void Writter(String fileName, int size, Boolean order)
+            {
             try
             {
 
-                StreamWriter sw = new StreamWriter("..\\..\\..\\Data\\" + fileName + ".txt", true);
+                StreamWriter sw = new StreamWriter("..\\..\\Data"+fileName+"\\.txt", true);
 
                 if (order)
                 {
                     int flag = 0;
-                    while (flag < size)
+                    while (flag<size)
                     {
-                        sw.Write(flag + " ");
-                        Console.WriteLine(value:flag);
+                        sw.WriteLine(flag+ " ");
                         flag++;
                     }
                 }
                 else
                 {
-                    while (size > 0)
+                    while (size>0)
                     {
-                        sw.Write(size + " ");
-                        Console.WriteLine(size+" ");
+                        sw.WriteLine(size + " ");
                         size--;
                     }
                 }
@@ -94,6 +86,6 @@ namespace writter
             {
                 Console.WriteLine("Exception: " + e.Message);
             }
-        }
+            }
     }
 }
